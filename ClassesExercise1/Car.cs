ad1feb8de31fac997f985e98f0a0cd4efbdc6e14
David Smith <cpmpdavidsmith@icloud.com>
2023-02-14 08:50:24 -0500

    CLassess Exercise 1

---
+++ ClassesExercise1/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ClassesExercise1
+{
+	public class Car
+	{
+		public Car()
+		{
+            
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        //YOU HAVE THE OPTION TO USE
+        //FIELDS
+        //PROPERTIES
+        //METHODS
+        //create a Make property of type string that is public 
+        //PROPERTIES
+        public string Make {get; set;}
+        public string Model {get; set;}
+		public int Year {get; set;}
+	}
+}
---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise1
+{   //within class I create a main method array 
+    class Program
+    {
+        static void Main(string[] args)
+        {//I choose which method to create the blueprint of the instance..dot notation ..??
+            Car car1 = new Car();
+            car1.Make = "Dodge";
+            car1.Model = "Challenger";
+            car1.Year = 2018;
+            //initializer...
+            var honda = new Car()
+            {
+                Make = "Honda",
+                Model = "Accord",
+                Year = 2020
+            };
+
+            var porcha = new Car("Porcha", "911", 2022);
+            //then I create a new list to then call..
+            var carList = new List<Car>() { car1, honda, porcha }; ;
+            foreach (var item in carList)
+            {
+                Console.WriteLine($"{item.Make} {item.Model} {item.Year}");
+      
+            }
+        }
+        
+            
+    }
+}
+
